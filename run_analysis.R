1588bdfe54c6a8d56f812f15fb343a021ee36784
makotolee <brandonlmc_24@hotmail.com>
2015-06-14 15:49:20 +0800

    Second update

--- run_analysis.R
+++ run_analysis.R
@@ -18,7 +18,7 @@ temp1 <- read.table("train\\y_train.txt")
 temp2 <- read.table("test\\y_test.txt")
 Y <- rbind(temp1, temp2)
 
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+# 2. Extracts the mean and standard deviation for each measurement.
 
 features <- read.table("features.txt")
 good_features <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
@@ -27,14 +27,14 @@ names(X) <- features[good_features, 2]
 names(X) <- gsub("\\(|\\)", "", names(X))
 names(X) <- tolower(names(X))
 
-# 3. Uses descriptive activity names to name the activities in the data set.
+# 3. Name the activities in the data set using descriptive activity names
 
 activities <- read.table("activity_labels.txt")
 activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
 Y[,1] = activities[Y[,1], 2]
 names(Y) <- "activity"
 
-# 4. Appropriately labels the data set with descriptive activity names.
+# 4. Labels the data set with descriptive activity names.
 
 names(S) <- "subject"
 cleaned_data <- cbind(S, Y, X)
@@ -42,20 +42,20 @@ write.table(cleaned_data, "merged_clean_data.txt")
 
 # 5. Creates a 2nd, independent tidy data set with the average of each variable for each activity and each subject.
 
-uniqueSubjects = unique(S)[,1]
-numSubjects = length(unique(S)[,1])
-numActivities = length(activities[,1])
-numCols = dim(cleaned_data)[2]
-result = cleaned_data[1:(numSubjects*numActivities), ]
+Subjects_unique = unique(S)[,1]
+Subjects_num = length(unique(S)[,1])
+num_Activities = length(activities[,1])
+num_Cols = dim(cleaned_data)[2]
+result = cleaned_data[1:(Subjects_num*num_Activities), ]
 
 row = 1
-for (s in 1:numSubjects) {
-    for (a in 1:numActivities) {
-        result[row, 1] = uniqueSubjects[s]
+for (s in 1:Subjects_num) {
+    for (a in 1:num_Activities) {
+        result[row, 1] = Subjects_unique[s]
         result[row, 2] = activities[a, 2]
         tmp <- cleaned_data[cleaned_data$subject==s & cleaned_data$activity==activities[a, 2], ]
-        result[row, 3:numCols] <- colMeans(tmp[, 3:numCols])
+        result[row, 3:num_Cols] <- colMeans(tmp[, 3:num_Cols])
         row = row+1
     }
 }
-write.table(result, "data_set_with_the_averages_final.txt",row.names=FALSE)
+write.table(result, "data_set_final.txt",row.names=FALSE)
